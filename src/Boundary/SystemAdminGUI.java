70d2b96d9954fb7db32f9779ad213a4057b3579a
marc <marcryanbritto@gmail.com>
2023-10-18 13:42:52 +0800

    SystemAdminGUI: added create account SystemAdminCreateAcc: added new class

--- src/Boundary/SystemAdminGUI.java
+++ src/Boundary/SystemAdminGUI.java
@@ -19,6 +19,11 @@ public class SystemAdminGUI {
         titleLabel.setBounds(100,20, 500, 25);
         panel.add(titleLabel);
 
+        // #3 SysAdmin Create account
+        JButton createAccButton = new JButton("Create Account");
+        createAccButton.setBounds(150, 50, 100, 25);
+        panel.add(createAccButton);
+
         // Logout Button
         JButton logoutButton = new JButton("Logout");
         logoutButton.setBounds(150, 175, 100, 25);
@@ -33,6 +38,12 @@ public class SystemAdminGUI {
         logoutButton.addActionListener(e -> {
             frame.dispose();
             new LoginGUI();
+        });
+
+        // Action for Create Account
+        createAccButton.addActionListener(e -> {
+            frame.dispose();
+            new SystemAdminCreateAcc();
         });
     }
 }
