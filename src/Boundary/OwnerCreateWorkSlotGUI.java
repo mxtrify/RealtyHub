01d63ee8d94068729666be38ea50b3ae2b5f8ccb
marc <marcryanbritto@gmail.com>
2023-10-18 22:05:09 +0800

    SystemAdminGUI: added create account SystemAdminCreateAcc: added new class

--- src/Boundary/OwnerCreateWorkSlotGUI.java
+++ src/Boundary/OwnerCreateWorkSlotGUI.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 public class OwnerCreateWorkSlotGUI {
     private JButton backButton;
     private JButton createButton;
+    private JTextField searchField;
     private JTextField dateField;
     private JTextField timeField;
     private JComboBox<String> roleField;
@@ -15,6 +16,7 @@ public class OwnerCreateWorkSlotGUI {
         displayCreateWorkSlotGUI();
     }
 
+    // Need each part to be an object?
     public void displayCreateWorkSlotGUI(){
         JFrame frame = new JFrame("Create Work Slot");
         JPanel panel = new JPanel();
@@ -22,11 +24,19 @@ public class OwnerCreateWorkSlotGUI {
 
         // Title Label
         JLabel titleLabel = new JLabel("Create Work Slot");
-        titleLabel.setBounds(100, 20, 500, 25);
+        titleLabel.setBounds(100, 10, 500, 25);
         panel.add(titleLabel);
 
+        // Search Bar
+        JLabel searchLabel = new JLabel("Search:");
+        searchLabel.setBounds(75, 35, 100, 25);
+        panel.add(searchLabel);
+        searchField = new JTextField(25);
+        searchField.setBounds(150, 35, 100, 25);
+        panel.add(searchField);
+
         // Date Label
-        JLabel dateLabel = new JLabel("Date: ");
+        JLabel dateLabel = new JLabel("Date:");
         dateLabel.setBounds(75, 75, 100, 25);
         panel.add(dateLabel);
 
@@ -36,7 +46,7 @@ public class OwnerCreateWorkSlotGUI {
         panel.add(dateField);
 
         // Time Label
-        JLabel timeLabel = new JLabel("Time: ");
+        JLabel timeLabel = new JLabel("Time:");
         timeLabel.setBounds(75, 115, 100, 25);
         panel.add(timeLabel);
 
@@ -46,7 +56,7 @@ public class OwnerCreateWorkSlotGUI {
         panel.add(timeField);
 
         //Role Label
-        JLabel roleLabel = new JLabel("Role: ");
+        JLabel roleLabel = new JLabel("Role:");
         roleLabel.setBounds(75, 155, 100, 25);
         panel.add(roleLabel);
 
@@ -57,7 +67,7 @@ public class OwnerCreateWorkSlotGUI {
         panel.add(roleField);
 
         // Amount Label
-        JLabel amountLabel = new JLabel("Amount: ");
+        JLabel amountLabel = new JLabel("Amount:");
         amountLabel.setBounds(75, 195, 100, 25);
         panel.add(amountLabel);
 
@@ -68,7 +78,7 @@ public class OwnerCreateWorkSlotGUI {
 
         // Create Button
         createButton = new JButton("Create");
-        createButton.setBounds(250, 220, 100, 25);
+        createButton.setBounds(300, 220, 100, 25);
         panel.add(createButton);
 
         // Back Button
@@ -86,7 +96,7 @@ public class OwnerCreateWorkSlotGUI {
         });
 
         frame.add(panel);
-        frame.setSize(400, 300);
+        frame.setSize(500, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
