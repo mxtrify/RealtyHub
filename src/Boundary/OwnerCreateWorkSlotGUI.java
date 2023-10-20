1c67953ecbaaf58b8521df3257e035e18e2fb19e
marc <marcryanbritto@gmail.com>
2023-10-20 23:16:03 +0800

    SystemAdminGUI: added create account SystemAdminCreateAcc: added new class

--- src/Boundary/OwnerCreateWorkSlotGUI.java
+++ src/Boundary/OwnerCreateWorkSlotGUI.java
@@ -13,10 +13,9 @@ public class OwnerCreateWorkSlotGUI {
     private JButton createButton;
     private JTextField searchField;
     private JTextField dateField;
-    private JDateChooser dateChosen;
-    private JTextField timeField;
-    private JComboBox<String> roleField;
-    private JTextField amountField;
+    private JTextField cashierField;
+    private JTextField chefField;
+    private JTextField staffField;
 
     public OwnerCreateWorkSlotGUI(UserAccount u){
         displayCreateWorkSlotGUI(u);
@@ -48,49 +47,46 @@ public class OwnerCreateWorkSlotGUI {
 
         // Date Field
         /*
-        dateChosen = new JDateChooser();
-        dateChosen.setDateFormatString("dd-MM-yyyy");
-
-        dateField = new JTextField(20);
-        dateField = setEditable(false);
-        panel.add(dateChosen);
-        panel.add(dateField);
+        JDateChooser dateChooser = new JDateChooser();
+        dateChooser.setDateFormatString("dd-MM-yyyy");
+        dateChooser.setBounds(150, 75, 175, 25);
+        panel.add(dateChooser);
 
         */ // Calendar Testing
         dateField = new JTextField(20);
         dateField.setBounds(150, 75, 175, 25);
         panel.add(dateField);
 
-        // Time Label
-        JLabel timeLabel = new JLabel("Time:");
-        timeLabel.setBounds(75, 115, 100, 25);
-        panel.add(timeLabel);
+        // Chef Label
+        JLabel chefLabel = new JLabel("Chef:");
+        chefLabel.setBounds(75, 115, 100, 25);
+        panel.add(chefLabel);
 
-        // Time Field
-        timeField = new JTextField(20);
-        timeField.setBounds(150, 115, 100, 25);
-        panel.add(timeField);
+        // Chef Field
+        chefField = new JTextField(20);
+        chefField.setBounds(150, 115, 100, 25);
+        panel.add(chefField);
 
-        //Role Label
-        JLabel roleLabel = new JLabel("Role:");
-        roleLabel.setBounds(75, 155, 100, 25);
-        panel.add(roleLabel);
 
-        //Role Field
-        String[] roles = {"Chef", "Waiter", "Cashier"};
-        roleField = new JComboBox<>(roles);
-        roleField.setBounds(150, 155, 100, 25);
-        panel.add(roleField);
+        // Cashier Label
+        JLabel cashierLabel = new JLabel("Cashier:");
+        cashierLabel.setBounds(75, 155, 100, 25);
+        panel.add(cashierLabel);
 
-        // Amount Label
-        JLabel amountLabel = new JLabel("Amount:");
-        amountLabel.setBounds(75, 195, 100, 25);
-        panel.add(amountLabel);
+        // Cashier Field
+        cashierField = new JTextField(20);
+        cashierField.setBounds(150, 155, 100, 25);
+        panel.add(cashierField);
 
-        // Amount Field
-        amountField = new JTextField(20);
-        amountField.setBounds(150, 195, 100, 25);
-        panel.add(amountField);
+        // Staff Label
+        JLabel staffLabel = new JLabel("Staff:");
+        staffLabel.setBounds(75, 195, 100, 25);
+        panel.add(staffLabel);
+
+        // Staff Field
+        staffField = new JTextField(20);
+        staffField.setBounds(150, 195, 100, 25);
+        panel.add(staffField);
 
         // Create Button
         createButton = new JButton("Create");
