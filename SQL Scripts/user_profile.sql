ae1c7e8001f34c4d56b8b3229da567b6aed12cbb
mxtrify <60342766+mxtrify@users.noreply.github.com>
2024-05-17 06:03:33 +0800

    Delete SQL Scripts/user_profile.sql

--- SQL Scripts/user_profile.sql
+++
@@ -1,4 +0,0 @@
-TRUNCATE TABLE `user_profile`;
-ALTER TABLE `user_profile` AUTO_INCREMENT = 1;
-
-INSERT INTO `user_profile` VALUES (1,'System Admin','System Admin',1),(2,'Real Estate Agent','Real Estate Agent',1),(3,'Buyer','Buyer',1),(4,'Seller','Seller',1);
